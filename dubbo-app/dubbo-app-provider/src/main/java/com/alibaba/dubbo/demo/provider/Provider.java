1dca558f154f8fefcc47c4062460f708fb040ba2
hucheng <hucheng@huchengdemacbook-pro.local>
2018-01-01 16:28:33 +0800

    完善项目模版

--- dubbo-app/dubbo-app-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ dubbo-app/dubbo-app-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -1,7 +1,5 @@
 package com.alibaba.dubbo.demo.provider;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Created by ken.lj on 2017/7/31.
  */
@@ -9,10 +7,6 @@ public class Provider {
 
     public static void main(String[] args) throws Exception {
         com.alibaba.dubbo.container.Main.main(args);
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/provider.xml"});
-//        context.start();
-//
-//        System.in.read(); // 按任意键退出
     }
 
 }
